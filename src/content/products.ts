e24888c8c252f125a7ea35071cd257e17dccf2f6
Shakil-Ahmmed-58464 <shakilahmmedwakil.saw@gmail.com>
2024-07-26 23:11:37 +0600

    added type of products

---
+++ src/content/products.ts
@@ -0,0 +1,73 @@
+type Product = {
+  id: number;
+  category: string;
+  name: string;
+  price: number;
+  image: string;
+};
+
+const products: Product[] = [
+  {
+    id: 1,
+    category: "Waffle",
+    name: "Waffle with Berries",
+    price: 6.5,
+    image: "image-waffle-desktop.jpg",
+  },
+  {
+    id: 2,
+    category: "Crème Brûlée",
+    name: "Vanilla Bean Crème Brûlée",
+    price: 7.0,
+    image: "image-creme-brulee-desktop.jpg",
+  },
+  {
+    id: 3,
+    category: "Macaron",
+    name: "Macaron Mix of Five",
+    price: 8.0,
+    image: "image-macaron-desktop.jpg",
+  },
+  {
+    id: 4,
+    category: "Tiramisu",
+    name: "Classic Tiramisu",
+    price: 5.5,
+    image: "image-tiramisu-desktop.jpg",
+  },
+  {
+    id: 5,
+    category: "Baklava",
+    name: "Pistachio Baklava",
+    price: 4.0,
+    image: "image-baklava-desktop.jpg",
+  },
+  {
+    id: 6,
+    category: "Pie",
+    name: "Lemon Meringue Pie",
+    price: 5.0,
+    image: "image-meringue-desktop.jpg",
+  },
+  {
+    id: 7,
+    category: "Cake",
+    name: "Red Velvet Cake",
+    price: 4.5,
+    image: "image-cake-desktop.jpg",
+  },
+  {
+    id: 8,
+    category: "Brownie",
+    name: "Salted Caramel Brownie",
+    price: 5.5,
+    image: "image-brownie-desktop.jpg",
+  },
+  {
+    id: 9,
+    category: "Panna Cotta",
+    name: "Vanilla Panna Cotta",
+    price: 6.5,
+    image: "image-panna-cotta-desktop.jpg",
+  },
+];
